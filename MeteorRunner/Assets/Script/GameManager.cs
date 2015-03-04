302fd8b5d3b31271a586c96dd8a92065032f9780
ChoKyungSung <van_01@hotmail.com>
2015-03-04 23:13:29 +0900

    high score

--- MeteorRunner/Assets/Script/GameManager.cs
+++ MeteorRunner/Assets/Script/GameManager.cs
@@ -3,11 +3,14 @@ using System.Collections;
 
 public class GameManager : MonoBehaviour {
 
+	static string HIGHSCORE = "HighScore";
+
 	private static GameObject container;  
 	private static GameManager m_instance = null;
 	private bool 				m_isGameOver;
-	private float 				m_fGameSpeed = 1.0f;
-	private float 				m_fScore = 0.0f;
+	private float 				m_fGameSpeed 	= 1.0f;
+	private float 				m_fScore 		= 0.0f;
+	private float				m_fHighScore 	= 0.0f;
 
 	public static GameManager Instance 
 	{
@@ -39,6 +42,12 @@ public class GameManager : MonoBehaviour {
 			{
 				Debug.Log ("GameOver!!");
 				m_fGameSpeed = 0.0f;
+
+				if (m_fHighScore < m_fScore)
+				{
+					m_fHighScore = m_fScore;
+					PlayerPrefs.SetFloat (HIGHSCORE, m_fHighScore);
+				}
 			}
 		}
 	}
@@ -65,9 +74,14 @@ public class GameManager : MonoBehaviour {
 		return m_fScore;
 	}
 
+	public float getHighScore()
+	{
+		return m_fHighScore;
+	}
+
 	// Use this for initialization
 	void Start () {
-
+		m_fHighScore = PlayerPrefs.GetFloat (HIGHSCORE, 0);
 	}
 	
 	// Update is called once per frame
--- MeteorRunner/Assets/Script/HighScoreLabel.cs
+++ MeteorRunner/Assets/Script/HighScoreLabel.cs
@@ -1,11 +1,10 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class ScoreLabel : MonoBehaviour {
+public class HighScoreLabel : MonoBehaviour {
 
-	private string 				m_FrontText = "Dist : ";
+	private string 				m_FrontText = "High : ";
 	private GUIText 			m_GuiScore;
-
 
 	// Use this for initialization
 	void Start () {
@@ -14,7 +13,7 @@ public class ScoreLabel : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		float fScore = GameManager.Instance.getScore ();
+		float fScore = GameManager.Instance.getHighScore ();
 		m_GuiScore.text = m_FrontText + fScore.ToString ("N2");
 	}
 }
