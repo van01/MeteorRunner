c7341349f22cd3882e2501e6e732ef9650f77a09
ChoKyungSung <van_01@hotmail.com>
2015-03-26 00:03:19 +0900

    count

---
+++ MeteorRunner/Assets/Script/UIReadyCount.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIReadyCount : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void gameStart()
+	{
+		GameManager.Instance.changeState (GameManager.STATE.STATE_GAME);
+	}
+}
