d83b065d7f0a08f6a4f9b6c89c848df99ed1d922
hackish7 <hackish@hotmail.com>
2015-03-30 23:45:08 +0900

    wtf

---
+++ MeteorRunner/Assets/Script/SoundEffects.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Creating instance of sounds from code with no effort
+/// </summary>
+public class SoundEffects : MonoBehaviour
+{	
+	/// <summary>
+	/// Singleton
+	/// </summary>
+	public static SoundEffects Instance;
+
+	public AudioClip Button;
+	public AudioClip ObjectDrop;
+	public AudioClip Damage;
+	
+	void Awake()
+	{
+		// Register the singleton
+		if (Instance != null)
+		{
+			Debug.LogError("Multiple instances of SoundEffectsHelper!");
+		}
+		Instance = this;
+	}
+	
+	public void MakeSound_Button()
+	{
+		MakeSound(Button);
+	}
+	
+	public void MakeSound_PObjectDrop()
+	{
+		MakeSound(ObjectDrop);
+	}
+	
+	public void MakeSound_Damage()
+	{
+		MakeSound(Damage);
+	}
+	
+	/// <summary>
+	/// Play a given sound
+	/// </summary>
+	/// <param name="originalClip"></param>
+	private void MakeSound(AudioClip originalClip)
+	{
+		// As it is not 3D audio clip, position doesn't matter.
+		AudioSource.PlayClipAtPoint(originalClip, transform.position);
+	}
+}
--- MeteorRunner/Assets/Script/UIButtonEvent.cs
+++ MeteorRunner/Assets/Script/UIButtonEvent.cs
@@ -9,8 +9,9 @@ public class UIButtonEvent : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update ()
+	{
+
 	}
 
 	public void onGameStart()
@@ -20,6 +21,18 @@ public class UIButtonEvent : MonoBehaviour {
 
 	public void onGameReload()
 	{
+		Application.LoadLevel("Ready");
+	}
+
+	public void onGamePlay()
+	{
+		SoundEffects.Instance.MakeSound_Button ();
+		Application.LoadLevel("Game");
+	}
+
+	public void onGameReady()
+	{
+		SoundEffects.Instance.MakeSound_Button ();
 		Application.LoadLevel("Ready");
 	}
 }
