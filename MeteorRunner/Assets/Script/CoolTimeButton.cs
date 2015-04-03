705e095eddc927b9a440cb8681ecca64aeaa4501
Jogyeongseong <notshade@gmail.com>
2015-04-03 18:08:05 +0900

    cool time button prefab 추가

--- MeteorRunner/Assets/Script/CoolTimeButton.cs
+++ MeteorRunner/Assets/Script/CoolTimeButton.cs
@@ -4,47 +4,85 @@ using System.Collections;
 
 public class CoolTimeButton : MonoBehaviour {
 
-	public Texture2D 	m_texture = null;
 	public Color		m_color;
+
+	private Transform	m_waitImageObject;
+	private RawImage 	m_waitImage;
+
+	private Button		m_button;
+
+	public Texture2D 	m_texture = null;
 	private Texture2D	m_progTex;
 	private float		m_progress = 0.0f;
 	private float		m_oldProgress = -1;
-
-	private bool 		m_isEnable			= true;
+	
 	private float		m_fCheckStartTime 	= 0.0f;
 	private float		m_fDisableTime		= 0.0f;
+	private float		m_fDelayTime 		= 3.0f;
 
 	// Use this for initialization
 	void Start () {
+
+		m_button = GetComponent<Button> (); 
+
+		m_waitImageObject = transform.Find ("Loading");
+		m_waitImage = m_waitImageObject.GetComponent<RawImage> ();
+		m_waitImage.texture = GetComponent<Image>().sprite.texture;
+
+		m_waitImageObject.GetComponent<RectTransform> ().sizeDelta = GetComponent<RectTransform> ().sizeDelta;
 		m_progTex = ProgressUpdate(m_progress, m_color);
-		setDisable (10.0f);
+		m_waitImage.texture = m_progTex;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-		if (!m_isEnable)
+		if (m_waitImage.enabled)
 		{
 			float deltaTime = Time.time - m_fCheckStartTime;
 			m_progress = deltaTime / m_fDisableTime;
+			if (m_progress > 1.0f)
+			{
+				setEnable();
+			}
 		}
 
 
 		if(m_oldProgress != m_progress){
 			m_progTex = ProgressUpdate(m_progress, m_color);
+			m_waitImage.texture = m_progTex;
 			m_oldProgress = m_progress;
 		}
 	}
 
+	public void setDelayTime (float time)
+	{
+		m_fDelayTime = time;
+	}
+
+	public void onClick ()
+	{
+		setDisable (m_fDelayTime);
+	}
+
 	void setDisable (float time)
 	{
-		m_isEnable = false;
+		m_waitImage.enabled = true;
+		m_button.interactable = !m_waitImage.enabled;
+
 		m_fCheckStartTime = Time.time;
 		m_fDisableTime = time;
 	}
 
+	void setEnable ()
+	{
+		m_waitImage.enabled = false;
+		m_button.interactable = !m_waitImage.enabled;
+	}
+
 	
 	Texture2D ProgressUpdate(float progress, Color overlayColor){
+
 
 		Texture2D thisTex = new Texture2D(m_texture.width, m_texture.height);
 
@@ -57,7 +95,7 @@ public class CoolTimeButton : MonoBehaviour {
 					angle += 360; //change angles to go from 0 to 360
 				}
 				Color pixColor = m_texture.GetPixel(x, y);
-				if(angle <= progress*360.0)
+				if(angle >= progress*360.0)
 				{ //if the angle is less than the progress angle blend the overlay colour
 					pixColor = new Color(
 						(pixColor.r*pixColor.a*(1-overlayColor.a))+(overlayColor.r*overlayColor.a),
@@ -65,18 +103,10 @@ public class CoolTimeButton : MonoBehaviour {
 						(pixColor.b*pixColor.a*(1-overlayColor.a))+(overlayColor.b*overlayColor.a)            
 						);
 				}
-
 				thisTex.SetPixel(x, y, pixColor);
 			}
 		}
 		thisTex.Apply(); //apply the cahnges we made to the texture
 		return thisTex;
 	}
-
-	void OnGUI(){
-		GUIStyle guiStyle  = new GUIStyle("button");
-		guiStyle.padding = new RectOffset(0, 0, 0, 0);
-		GUI.Button(new Rect(10, 10, 32, 32), m_progTex, guiStyle);
-	}
-
 }
