6da15bc8b1683a25912e0d058694d058eb09a278
Jogyeongseong <notshade@gmail.com>
2015-04-10 16:19:59 +0900

    add jerkins build option

---
+++ MeteorRunner/Assets/Editor/ProjectBuilder.cs
@@ -0,0 +1,37 @@
+﻿
+using UnityEngine;
+using UnityEditor;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+
+class ProjectBuilder {
+	static string[] SCENES = FindEnabledEditorScenes();
+	static string APP_NAME = "YourProject";
+	
+	[MenuItem ("Custom/CI/Build Android")]
+	static void PerformAndroidBuild ()
+	{
+		string target_filename = APP_NAME + ".apk";
+		GenericBuild(SCENES, target_filename, BuildTarget.Android ,BuildOptions.None);
+	}
+	
+	private static string[] FindEnabledEditorScenes() {
+		List<string> EditorScenes = new List<string>();
+		foreach(EditorBuildSettingsScene scene in EditorBuildSettings.scenes) {
+			if (!scene.enabled) continue;
+			EditorScenes.Add(scene.path);
+		}
+		return EditorScenes.ToArray();
+	}
+	
+	static void GenericBuild(string[] scenes, string target_filename, BuildTarget build_target, BuildOptions build_options)
+	{
+		EditorUserBuildSettings.SwitchActiveBuildTarget(build_target);
+		string res = BuildPipeline.BuildPlayer(scenes, target_filename, build_target, build_options);
+		if (res.Length > 0) {
+			throw new Exception("BuildPlayer failure: " + res);
+		}
+	}
+}
